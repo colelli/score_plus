6c365077a242fd626a09a071f2c76ed254ea1401
Lorenzo Colelli <lorenzo.colelli@protonmail.com>
2024-04-22 23:46:12 +0200

    complete overhaul

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,20 @@
 import './css/scoreplus.scss'
+import Navbar from './elements/Navbar'
+import Sidebar from './elements/Sidebar'
 
 var send = "Invia"
 var search = "Cerca"
+var file_upload = "File Upload"
+var drag_and_drop = "Drag and Drop file"
 
 function App() {
 
   return(
-    <div>
-      <nav className='navbar bg-primary-500 shadow'>
-        <div className='container-fluid'>
-          <a className='navbar-brand'>
-            <img className='logo' src='src/assets/score_plus_logo.svg'></img>
-          </a>
-          <form className='d-flex' role='search'>
-            <input className='form-control me-2' type='search' placeholder={search} aria-label='search'/>
-            <button className='btn btn-outlinesuccess' type='submit'>{search}</button>
-          </form>
-        </div>
-      </nav>
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='sidebar bg-primary-400 col-md-3 col-lg-2'>
-            <div className='offcanvas-md offcanvas-start show' tabIndex="-1" id="sidebar" aria-labelledby='sidebar'>
-              <div className='offcanvas-header'>
-                <h5 className='offcanvas-title' id='sidebarLabel'>Score+</h5>
-                <button type='button' className='btn-close' data-bs-dismiss='offcanvas' aria-label='close'></button>
-              </div>
-              <div className='offcanvas-body overflow-y-auto p-0 d-md-flex flex-column'>
-                <ul className='nav flex-column'>
-                  <li className='nav-item'>
-                    <a className='nav-link active' aria-current='page' href='#'>Dashboard</a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link align-items-center d-flex gap-2' href='#'>
-                      <img className='bi' src='assets/img.png'></img>
-                      Other
-                    </a>
-                  </li>
-                  <li className='nav-item'>
-                    <a className='nav-link' href='#'>Other</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <main className='col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-primary-700'>
-            <div className='container-fluid d-flex'>
-              <div className='input-group rounded p-3 mb-5 bg-body-tertiary align-items-center d-flex'>
-                <input type='text' className='form-control' placeholder='Insert CVE-ID'></input>
-                <button type='button' className='btn' datatype='inverted'>{send}</button>
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
-
+    <div className='w-screen bg-primary-700'>
+      <Navbar></Navbar>
+      <main className='bg-primary-500 h-[93vh]'>
+        <Sidebar current='dashboard'></Sidebar>
+      </main>
     </div>
   )
 
---
+++ src/elements/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { Disclosure } from '@headlessui/react'
+import { Fragment } from 'react'
+import { ComputerDesktopIcon, ClipboardIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+const sidebar_items = [
+    {name: 'Dashboard', href: '#', icon: <ComputerDesktopIcon className='h-6 w-6' aria-hidden='true'/>, id: 'dashboard'},
+    {name: 'History', href: '#', icon: <ClipboardIcon className='h-6 w-6' aria-hidden='true'/>, id: 'history'},
+    {name: 'Search', href: '#', icon: <MagnifyingGlassIcon className='h-6 w-6' aria-hidden='true'/>, id: 'search'}
+]
+
+export default function Sidebar(params) {
+    return (
+        <nav as='nav' className="bg-primary-700 h-full w-1/4 lg:w-2/12 min-w-48">
+            <ul className='flex flex-col items-center pt-5 w-full gap-3'>
+                {sidebar_items.map(
+                    (item) => 
+                    <a href={item.href} className={classNames(params.current == item.id ? 'bg-secondary-700 pointer-events-none current-item' : 'sidebar-item', 
+                    'rounded w-[90%] h-[5vh] font-semibold flex items-center p-5')}>
+                        <li className='flex gap-2'>
+                            {item.icon}
+                            {item.name}
+                        </li>
+                    </a>
+                )}
+            </ul>
+        </nav>
+    )
+  }
